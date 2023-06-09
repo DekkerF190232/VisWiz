3780fc291c826d53f89904040cb59a8d16b513db
Sirvierl0ffel <iminternetimmeranonym@gmail.com>
2023-06-09 14:41:36 +0200

    Android Studio Commit

---
+++ app/src/main/java/de/sirvierl0ffel/viswiz/MainActivity.java
@@ -0,0 +1,14 @@
+package de.sirvierl0ffel.viswiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
